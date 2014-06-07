4641acc8adcfcd4023a06f3cddbc87d5242f2b85
Adar <adark3@gmail.com>
2014-06-08 00:06:58 +0300

    Added basic GUI

--- SpeakApp/SpeakApp/src/main/java/com/speakapp/app/Main.java
+++ SpeakApp/SpeakApp/src/main/java/com/speakapp/app/Main.java
@@ -2,14 +2,35 @@ package com.speakapp.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.GridView;
+
+import java.util.ArrayList;
 
 
 public class Main extends Activity {
 
+    private ArrayList<String> m_activeBoard;
+    private BoardAdapter m_boardAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        init();
         setContentView(R.layout.activity_main);
+        GridView gridview = (GridView) findViewById(R.id.gridView);
+        gridview.setAdapter(m_boardAdapter);
+    }
+
+    private void init() {
+        m_activeBoard = new ArrayList<String>();
+        fillDemoValues(); // temp
+        m_boardAdapter = new BoardAdapter(this, m_activeBoard);
+    }
+
+    private void fillDemoValues() {
+        for(int i = 0; i < 10; i++) {
+            m_activeBoard.add("card " + i);
+        }
     }
 }
 
