d32344d7de8f4690f9732adeab3a1efc4585b51f
Adar <adark3@gmail.com>
2014-06-22 21:38:45 +0300

    yoyo

--- SpeakApp/SpeakApp/src/main/java/com/speakapp/app/general/SpeakAppApplication.java
+++ SpeakApp/SpeakApp/src/main/java/com/speakapp/app/general/SpeakAppApplication.java
@@ -7,6 +7,8 @@ public class SpeakAppApplication extends Application
     @Override
     public void onCreate()
     {
+        int grutaha = 5;
+        return;
        // SoundManager.initInstance(getApplicationContext());
     }
 }
